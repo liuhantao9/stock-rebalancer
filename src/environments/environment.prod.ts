c3b7603fc5d5cb72080e8e81b9e31d0bf2bb1a44
liuhantao9 <liuhantao9@gmail.com>
2021-05-27 13:21:48 -0700

    adding env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  'X-RAPIDAPI-KEY': '',
-  'X-RAPIDAPI-HOST': '',
+  'X-RAPIDAPI-KEY': 'b4ad6df1ebmsha41564313dcd338p185ff1jsn71767a01f781',
+  'X-RAPIDAPI-HOST': 'apidojo-yahoo-finance-v1.p.rapidapi.com',
   production: true,
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,6 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  'X-RAPIDAPI-KEY': 'b4ad6df1ebmsha41564313dcd338p185ff1jsn71767a01f781',
-  'X-RAPIDAPI-HOST': 'apidojo-yahoo-finance-v1.p.rapidapi.com',
   production: false,
 };
 
