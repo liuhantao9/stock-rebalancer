7dca16548c55e70d1823a363a81119ac79a32823
liuhantao9 <liuhantao9@gmail.com>
2021-05-27 00:12:17 -0700

    changing ignore

--- src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  'X-RAPIDAPI-KEY': 'b4ad6df1ebmsha41564313dcd338p185ff1jsn71767a01f781',
-  'X-RAPIDAPI-HOST': 'apidojo-yahoo-finance-v1.p.rapidapi.com',
-  production: true
-};
