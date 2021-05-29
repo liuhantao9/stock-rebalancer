1ec1b04cb31d13ace085d34c928c9582f9cba08a
liuhantao9 <liuhantao9@gmail.com>
2021-05-29 00:30:07 -0700

    adding routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { StockComponentCalculatorComponent } from './component/stock-component-calculator/stock-component-calculator.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: StockComponentCalculatorComponent },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    useHash: false,
+    anchorScrolling: 'enabled',
+    enableTracing: true   // <-- debugging purposes
+  })],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
