098ace20cb2ce4b61883951c18f86f8790528605
liuhantao9 <liuhantao9@gmail.com>
2022-12-02 14:46:23 -0800

    changing the percentage

--- src/app/component/stock-component-calculator/stock-component-calculator.component.ts
+++ src/app/component/stock-component-calculator/stock-component-calculator.component.ts
@@ -26,7 +26,7 @@ export class StockComponentCalculatorComponent implements OnInit {
 
   results: Stock[] = [];
   initNames = ['QQQ', 'VOO', 'FXI', 'VEA', 'TIP', 'TLT', 'INTC', 'NIO'];
-  initPercentage = [25, 18, 15, 12, 20, 5, 3, 2];
+  initPercentage = [25, 20, 18, 12, 15, 5, 3, 2];
   remainBP: number;
 
   ngOnInit(): void {
